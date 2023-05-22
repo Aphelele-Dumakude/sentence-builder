049e601bbc88932dbfa4a0460f11d5faf539e5b4
Aphelele-Dumakude <apheleledumakude@gmail.com>
2023-05-22 12:32:26 +0200

    added required dependencies on POM xml file

---
+++ src/main/java/com/runninghill/sentencebuilder/SentenceBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.runninghill.sentencebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceBuilderApplication.class, args);
+	}
+
+}
